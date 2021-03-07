cc3966d5a0dcc637d223e10a000cfe3ee935477f
Pavel <>
2021-03-06 16:41:41 -0800

    modifiable menu items

--- ajidex-client/src/Restaurant.js
+++ ajidex-client/src/Restaurant.js
@@ -4,7 +4,7 @@ import Salad from './img/potato_salad_template.jpg';
 
 const testSalad = {
   Name: "Sweet Potato Salad",
-  Description: "A sweet potato salad with only the finest leafy lettuce and thick cut sweet potatoes. Our chef&#39;s finest creation in the world. Honestly idk how he does it.",
+  Description: "A sweet potato salad with only the finest leafy lettuce and thick cut sweet potatoes. Our chef's finest creation in the world. Honestly idk how he does it.",
   Ingredients: ["Sweet Potato", "Lettuce"],
   Calories: 400,
   Textures: ["Fibrous", "Spongy"],
@@ -22,6 +22,7 @@ function Restaurant(props) {
       <div className="menu">
         <h2>Appetizers</h2>
         <MenuItem Name={testSalad.Name} Description={testSalad.Description} Ingredients={testSalad.Ingredients} Calories={testSalad.Calories} Textures={testSalad.Textures} Diets={testSalad.Diets} Image={testSalad.Image}/>
+        <MenuItem Name="Pork Soda" Description="Easy on the tongue" Ingredients={testSalad.Ingredients} Calories={200} Textures={["Smooth, like jazz"]} Diets={["Keto"]} Image="https://ih1.redbubble.net/image.418980247.7118/flat,128x128,075,t-pad,128x128,f8f8f8.u3.jpg" />
         <h2>Meals</h2>
       </div>
     </div>
