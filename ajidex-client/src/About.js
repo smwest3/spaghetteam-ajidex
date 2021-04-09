6450d47bd2530e234d43ec2fcc6895833c7be4a3
Nikki Demmel <ndemmel@uw.edu>
2021-04-09 11:04:03 -0700

    Add about section

--- ajidex-client/src/About.js
+++ ajidex-client/src/About.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function About() {
   return (
-    <div>This is the about page</div>
+    <div>
+        <p>AjiDex is a 2020-2021 capstone project for the University of Washington iSchool created by Sarah West, Meesha Hussain, Pavel Batalov, and Nikki Demmel. 
+           This web app aims to help people with food texture sensitivities make informed choices on the meals they order at restaurants to reduce food waste and 
+           save money.
+        </p>
+    </div>
   );
 }
 
